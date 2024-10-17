c78ce7b96121bb9986a2a82821e5a00bb1d13349
santitroconis <santitroconis@gmail.com>
2024-10-17 12:22:33 -0400

    dbConfig changes: personal dbb and gitignore

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,7 +3,7 @@ module.exports = {
     user: "postgres",
     host: "localhost",
     database: "test",
-    password: "davidpaz06",
+    password: "postgres",
     port: 5432,
     ssl: false,
     max: 20,
@@ -11,17 +11,4 @@ module.exports = {
     connectionTimeoutMillis: 2000,
     maxUses: 7500,
   },
-
-  // dbConfig: {
-  //   user: "postgres",
-  //   host: "localhost",
-  //   database: "test",
-  //   password: "postgres",
-  //   port: 5432,
-  //   ssl: false,
-  //   max: 20,
-  //   idleTimeoutMillis: 30000,
-  //   connectionTimeoutMillis: 2000,
-  //   maxUses: 7500,
-  // },
 };
