3531e296664bc8ad7e56e44e7e6e922444f1a359
santitroconis <santitroconis@gmail.com>
2024-10-17 09:21:18 -0400

    Troco DBB (commented)

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -13,6 +13,19 @@ module.exports = {
   },
 
   // dbConfig: {
+  //   user: "postgres",
+  //   host: "localhost",
+  //   database: "test",
+  //   password: "postgres",
+  //   port: 5432,
+  //   ssl: false,
+  //   max: 20,
+  //   idleTimeoutMillis: 30000,
+  //   connectionTimeoutMillis: 2000,
+  //   maxUses: 7500,
+  // },
+
+  // dbConfig: {
   //   user: "postgres.meptjduhouhxagaqcmoy",
   //   host: "aws-0-us-west-1.pooler.supabase.com",
   //   database: "postgres",
