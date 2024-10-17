9f6f39a3fa0f987dee5d64111ac6f74a400fdc08
santitroconis <santitroconis@gmail.com>
2024-10-17 09:24:09 -0400

    Supabase DBB deleted

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -24,17 +24,4 @@ module.exports = {
   //   connectionTimeoutMillis: 2000,
   //   maxUses: 7500,
   // },
-
-  // dbConfig: {
-  //   user: "postgres.meptjduhouhxagaqcmoy",
-  //   host: "aws-0-us-west-1.pooler.supabase.com",
-  //   database: "postgres",
-  //   password: "4uXeeJmu3SkV3gZ6",
-  //   port: 6543,
-  //   ssl: false,
-  //   max: 20,
-  //   idleTimeoutMillis: 30000,
-  //   connectionTimeoutMillis: 2000,
-  //   maxUses: 7500,
-  // },
 };
